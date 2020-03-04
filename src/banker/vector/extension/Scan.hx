66cf2dcd9fea74c0f783ebc1220f2964357e48e4
FAL <falworks.contact@gmail.com>
2020-03-04 19:51:28 +0900

    vector: add join()

--- src/banker/vector/extension/Scan.hx
+++ src/banker/vector/extension/Scan.hx
@@ -26,4 +26,36 @@ class Scan {
 
 		return true;
 	}
+
+	/**
+		Joins elements of `this` and returns a `String` representation.
+	**/
+	#if !banker_generic_disable
+	@:generic
+	#end
+	public static function joinUntil<T>(
+		_this: VectorReference<T>,
+		endIndex: Int,
+		separator: String
+	): String {
+		final buffer = new StringBuf();
+		buffer.add(_this[0]);
+
+		for (i in 1...endIndex) {
+			buffer.add(separator);
+			buffer.add(_this[i]);
+		}
+
+		return buffer.toString();
+	}
+
+	/**
+		Joins elements of `this` and returns a `String` representation.
+	**/
+	#if !banker_generic_disable
+	@:generic
+	#end
+	public static function join<T>(_this: VectorReference<T>, separator: String): String {
+		return joinUntil(_this, _this.length, separator);
+	}
 }
