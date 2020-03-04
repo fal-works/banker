1253c6071bc0e2ef750f43a9375a1e54894771d0
FAL <falworks.contact@gmail.com>
2020-03-05 01:46:54 +0900

    remove unused code

--- src/banker/vector/extension/Functional.hx
+++ src/banker/vector/extension/Functional.hx
@@ -172,39 +172,3 @@ class Functional {
 		forEachIndexIn(_this, callback, 0, _this.length);
 	}
 }
-
-class WritableFunctional {
-	/**
-		@see `Functional.forEachIndexIn()`
-	**/
-	public static inline function forEachIndexIn<T>(
-		_this: WritableVector<T>,
-		callback: (
-			element: T,
-			index: Int,
-			vector: WritableVector<T>
-		) -> Void,
-		startIndex: Int,
-		endIndex: Int
-	): Void {
-		var i = startIndex;
-		while (i < endIndex) {
-			callback(_this[i], i, _this);
-			++i;
-		}
-	}
-
-	/**
-		@see `Functional.forEachIndex()`
-	**/
-	public static inline function forEachIndex<T>(
-		_this: WritableVector<T>,
-		callback: (
-			element: T,
-			index: Int,
-			vector: WritableVector<T>
-		) -> Void
-	): Void {
-		forEachIndexIn(_this, callback, 0, _this.length);
-	}
-}
