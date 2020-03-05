8be87aa269d7542f9a363a5fd2b7f8e663c0b463
FAL <falworks.contact@gmail.com>
2020-03-05 20:25:20 +0900

    add test case

--- test/ArraySetTest.hx
+++ test/ArraySetTest.hx
@@ -12,6 +12,7 @@ class ArraySetTest {
 		set.add("DDD");
 		set.remove("BBB");
 		assert(set.toString() == "AAA, DDD, CCC");
+		println("data: " + set.toString());
 	}
 
 	static final _remove = testCase(remove, Ok);
@@ -26,12 +27,32 @@ class ArraySetTest {
 		set.add(5);
 		set.removeAll(n -> n == 2 || n == 4);
 		assert(set.toString() == "1, 5, 3");
+		println("data: " + set.toString());
 	}
 
 	static final _removeAll = testCase(removeAll, Ok);
 
+	static function forEach() {
+		describe();
+		final set = new ArraySet<Int>(5);
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		set.remove(2);
+		set.add(5);
+		set.add(2);
+		var sum = 0;
+		set.forEach(n -> sum += n);
+		assert(sum == 15);
+		println("data: " + set.toString());
+	}
+
+	static final _forEach = testCase(forEach, Ok);
+
 	public static final all = testCaseGroup([
 		_remove,
-		_removeAll
+		_removeAll,
+		_forEach
 	]);
 }
