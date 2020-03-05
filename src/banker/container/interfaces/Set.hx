f7f34ee2e9da8275ddb46c04ea8e0d62c237a5b3
FAL <falworks.contact@gmail.com>
2020-03-05 21:33:19 +0900

    container: add comments

--- src/banker/container/interfaces/Set.hx
+++ src/banker/container/interfaces/Set.hx
@@ -1,10 +1,35 @@
 package banker.container.interfaces;
 
 interface Set<T> extends Sequence<T> {
+	/**
+		Adds `value` to `this`.
+	**/
 	function add(value: T): Void;
+
+	/**
+		@return The first found element that matches `predicate`. If not found, `defaultValue`.
+	**/
 	function findFirst(predicate: (element: T) -> Bool, defaultValue: T): T;
+
+	/**
+		Finds and removes `value` from `this`.
+		@return `true` if found and removed.
+	**/
 	function remove(value: T): Bool;
+
+	/**
+		Removes all elements that match `predicate`.
+		@return `true` if any found and removed.
+	**/
 	function removeAll(predicate: (element: T) -> Bool): Bool;
+
+	/**
+		@return `true` if `this` contains `value`.
+	**/
 	function has(value: T): Bool;
+
+	/**
+		@return `ture` if `this` contains any element that matches `predicate`.
+	**/
 	function hasAny(predicate: (element: T) -> Bool): Bool;
 }
