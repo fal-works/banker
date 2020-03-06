d2d80d5584dd501c385e0bf4f7c91e709509a388
FAL <falworks.contact@gmail.com>
2020-03-07 03:15:51 +0900

    add test cases

--- test/ArrayStackTest.hx
+++ test/ArrayStackTest.hx
@@ -34,9 +34,47 @@ class ArrayStackTest {
 
 	static final _emptyError = testCase(emptyError, Fail);
 
+
+	static function clone() {
+		describe("This raises an exception if assertion is enabled.");
+		final stack = new ArrayStack<String>(5);
+		stack.push("AAA");
+		stack.push("BBB");
+		stack.push("CCC");
+		stack.pop();
+
+		final cloned = stack.cloneAsStack();
+		assert(stack.capacity == cloned.capacity);
+		assert(stack.size == cloned.size);
+		assert(stack.toString() == cloned.toString());
+
+		println('source: ${stack.toString()}\ncloned: ${cloned.toString()}');
+	}
+
+	static final _clone = testCase(clone, Ok);
+
+	static function cloneResized() {
+		describe("This raises an exception if assertion is enabled.");
+		final stack = new ArrayStack<String>(5);
+		stack.push("AAA");
+		stack.push("BBB");
+		stack.push("CCC");
+
+		final cloned = stack.cloneAsStack(2);
+		println('source: ${stack.toString()}\ncloned: ${cloned.toString()}');
+
+		assert(cloned.capacity == 2);
+		assert(cloned.size == 2);
+		assert(cloned.toString() == "AAA, BBB");
+	}
+
+	static final _cloneResized = testCase(cloneResized, Ok);
+
 	public static final all = testCaseGroup([
 		_push,
 		_pop,
-		_emptyError
+		_emptyError,
+		_clone,
+		_cloneResized
 	]);
 }
