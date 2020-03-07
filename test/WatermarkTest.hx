86e3a321251c7496f0186d90fe9973e6d07694f4
FAL <falworks.contact@gmail.com>
2020-03-07 22:00:23 +0900

    watermark: add test case

--- test/WatermarkTest.hx
+++ test/WatermarkTest.hx
@@ -3,11 +3,14 @@ package;
 using sneaker.tag.TaggedExtension;
 
 import banker.watermark.Watermark;
+import banker.watermark.WatermarkSettings;
 import banker.container.ArrayStack;
 
 class WatermarkTest {
 	static function watermark() {
 		describe("This prints watermark result (A: 50%, B: 75%) if `banker_watermark_enabled` is set.");
+
+		Watermark.reset();
 
 		final stackA1 = new ArrayStack<Int>(10).newTag("stack group A");
 		for (i in 0...2) stackA1.push(i);
@@ -25,7 +28,29 @@ class WatermarkTest {
 
 	static final _watermark = testCase(watermark, Visual);
 
+	static function exclude() {
+		describe("This prints result if `banker_watermark_enabled` is set, but only for stack 2.");
+
+		Watermark.reset();
+
+		final bitMask = 0x00001111;
+		WatermarkSettings.excludeTagBits = (bits: Int) -> bits & bitMask > 0;
+		WatermarkSettings.usageDataMapBits = bitMask;
+
+		final stack1 = new ArrayStack<Int>(10).newTag("stack 1", 0x00000101);
+		for (i in 0...3) stack1.push(i);
+
+		final stack2 = new ArrayStack<Int>(10).newTag("stack 2", 0x11110000);
+		for (i in 0...5) stack2.push(i);
+
+		Watermark.printData();
+	}
+
+	static final _exclude = testCase(exclude, Visual);
+
+
 	public static final all = testCaseGroup([
-		_watermark
+		_watermark,
+		_exclude
 	]);
 }
