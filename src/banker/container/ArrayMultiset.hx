ba716adc99b2d3916d9c73d180eb5ed7f3437735
FAL <falworks.contact@gmail.com>
2020-03-08 04:51:23 +0900

    container: fix ArrayMultiSet => ArrayMultiset

---
+++ src/banker/container/ArrayMultiset.hx
@@ -0,0 +1,43 @@
+package banker.container;
+
+// NOTE: Automatic static extension does not work on generic classes
+import banker.container.buffer.top_aligned.*;
+
+/**
+	Array-based set.
+	Suited for iteration or holding small number of elements.
+	The order is not preserved.
+	Allows duplicates.
+**/
+#if !banker_generic_disable
+@:generic
+#end
+class ArrayMultiset<T> extends TopAlignedSetBuffer<T> {
+	/** @inheritdoc **/
+	public function new(capacity: Int)
+		super(capacity);
+
+	/**
+		@see `banker.container.buffer.top_aligned.TopAlignedBuffer`
+		@see `banker.container.buffer.top_aligned.InternalExtension`
+	**/
+	override inline function pushInternal(index: Int, element: T): Void
+		InternalExtension.pushDuplicatesAllowed(this, index, element);
+
+	/**
+		@see `banker.container.buffer.top_aligned.TopAlignedBuffer`
+		@see `banker.container.buffer.top_aligned.InternalExtension`
+	**/
+	override inline function pushFromVectorInternal(
+		index: Int,
+		otherVector: VectorReference<T>,
+		otherVectorLength: Int
+	): Void {
+		InternalExtension.pushFromVectorDuplicatesAllowed(
+			this,
+			index,
+			otherVector,
+			otherVectorLength
+		);
+	}
+}
