8b5b0156ade3482e83aba277bce9bd3957ee5c92
FAL <falworks.contact@gmail.com>
2020-03-24 12:55:16 +0900

    linker: refactor/improve LinkerBuilder

--- src/banker/linker/LinkerBuilder.hx
+++ src/banker/linker/LinkerBuilder.hx
@@ -1,45 +1,56 @@
 package banker.linker;
 
+using banker.type_extension.MapExtension;
+
+import haxe.ds.Map as StdMap;
+import banker.linker.buffer.top_aligned.TopAlignedBuffer;
+
 class LinkerBuilder {
 	/**
 		@param capacity Max number of key-value pairs that can be contained.
+		if negative, the number of `map` keys is used.
 		@return New `ArrayMap` instance created from `map`.
 	**/
 	public static function arrayMapFromStandardMap<K, V>(
-		map: haxe.ds.Map<K, V>,
+		map: StdMap<K, V>,
 		capacity: Int
 	) {
-		final arrayMap = new ArrayMap<K, V>(capacity);
-		final keys = arrayMap.keyVector;
-		final values = arrayMap.valueVector;
-		var i = 0;
-		for (key => value in map) {
-			keys[i] = key;
-			values[i] = value;
-			++i;
-		}
-		arrayMap.nextFreeSlotIndex = i;
+		final length = if (capacity >= 0) capacity else map.countKeys();
+		final arrayMap = new ArrayMap<K, V>(length);
+		blitFromMapToBuffer(map, arrayMap);
 		return arrayMap;
 	}
 
 	/**
 		@param capacity Max number of key-value pairs that can be contained.
+		if negative, the number of `map` keys is used.
 		@return New `ArrayOrderedMap` instance created from `map`.
 	**/
 	public static function arrayOrderedMapFromStandardMap<K, V>(
-		map: haxe.ds.Map<K, V>,
+		map: StdMap<K, V>,
 		capacity: Int
 	) {
-		final arrayMap = new ArrayOrderedMap<K, V>(capacity);
-		final keys = arrayMap.keyVector;
-		final values = arrayMap.valueVector;
+		final length = if (capacity >= 0) capacity else map.countKeys();
+		final arrayMap = new ArrayOrderedMap<K, V>(length);
+		blitFromMapToBuffer(map, arrayMap);
+		return arrayMap;
+	}
+
+	/**
+		Overrites `buffer` with all key-value entries of `map`.
+	**/
+	static inline function blitFromMapToBuffer<K, V>(
+		map: StdMap<K, V>,
+		buffer: TopAlignedBuffer<K, V>
+	): Void {
+		final keys = buffer.keyVector;
+		final values = buffer.valueVector;
 		var i = 0;
 		for (key => value in map) {
 			keys[i] = key;
 			values[i] = value;
 			++i;
 		}
-		arrayMap.nextFreeSlotIndex = i;
-		return arrayMap;
+		buffer.nextFreeSlotIndex = i;
 	}
 }
