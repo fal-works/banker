297ad9877289bb3d17463632ebcd8f07d4707a65
FAL <falworks.contact@gmail.com>
2020-03-30 15:47:34 +0900

    aosoa: add Aosoa interface

---
+++ src/banker/aosoa/interfaces/Aosoa.hx
@@ -0,0 +1,10 @@
+package banker.aosoa.interfaces;
+
+/**
+	Interface that can be used for AoSoA classes generated from `Structure` classes.
+
+	Note that this is not automatically implemented.
+**/
+interface Aosoa {
+	function synchronize(): Void;
+}
