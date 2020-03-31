3cf2e734208ebedf63c25f435be96f661b9ffdfa
FAL <falworks.contact@gmail.com>
2020-03-31 23:08:19 +0900

    aosoa: add interface AosoaConstructible

---
+++ src/banker/aosoa/interfaces/AosoaConstructible.hx
@@ -0,0 +1,25 @@
+package banker.aosoa.interfaces;
+
+/**
+	Type for constructing AoSoA.
+
+	Usage example:
+
+	```haxe
+	@:generic
+	static function createAosoa<T:AosoaConstructible>():T {
+		final chunkCapacity = 64;
+		final chunkCount = 16;
+		final aosoa = new T(chunkCapacity, chunkCount);
+		return aosoa;
+	}
+
+	static function main() {
+		final aosoa: MyAosoaClass = createAosoa();
+	}
+	```
+**/
+typedef AosoaConstructible = haxe.Constraints.Constructible<(
+	chunkCapacity: Int,
+	chunkCount: Int
+) -> Void>;
