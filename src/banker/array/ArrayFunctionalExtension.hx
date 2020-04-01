b95787a69e48cb6b216b1aab59a9cbb183ecd24d
FAL <falworks.contact@gmail.com>
2020-04-01 14:28:02 +0900

    array: rename map() => allocateMap()

--- src/banker/array/ArrayFunctionalExtension.hx
+++ src/banker/array/ArrayFunctionalExtension.hx
@@ -194,9 +194,10 @@ class ArrayFunctionalExtension {
 
 	/**
 		An alternative to the standard `map()`.
+		It first allocates a new array with the entire length and then assigns mapped values.
 		@return New array with elements that are mapped from `array` by `callback`.
 	**/
-	public static inline function map<T, S>(_this: Array<T>, callback: T->S): Array<S> {
+	public static inline function allocateMap<T, S>(_this: Array<T>, callback: T->S): Array<S> {
 		final len = _this.length;
 		final newArray = ArrayTools.allocate(len);
 		var i = 0;
