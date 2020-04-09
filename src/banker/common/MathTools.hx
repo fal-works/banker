2612b2702ec61b2ccd38778a7ad31516e0c74ec3
FAL <falworks.contact@gmail.com>
2020-04-09 11:36:46 +0900

    common: add absoluteInt()

--- src/banker/common/MathTools.hx
+++ src/banker/common/MathTools.hx
@@ -6,4 +6,7 @@ class MathTools {
 
 	public static inline function maxInt(a: Int, b: Int): Int
 		return if (a < b) b else a;
+
+	public static inline function absoluteInt(n: Int): Int
+		return if (0 <= n) n else -n;
 }
