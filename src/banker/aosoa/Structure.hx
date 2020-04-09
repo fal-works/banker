f700fcc676786c97c81b3c9df5d261f42c42b498
FAL <falworks.contact@gmail.com>
2020-04-10 01:43:35 +0900

    aosoa: fix comment of Structure

--- src/banker/aosoa/Structure.hx
+++ src/banker/aosoa/Structure.hx
@@ -3,8 +3,6 @@ package banker.aosoa;
 /**
 	Marker interface indicating that the class is a structure from which an AoSoA
 	(Array of Structure of Arrays) class is to be generated.
-
-	By implementing this, the class will have a static method `createAosoa()`.
 **/
 @:autoBuild(banker.aosoa.macro.Builder.structure())
 interface Structure {}
