69535c48066360c8dc80a5eacb5763c5813491fb
FAL <falworks.contact@gmail.com>
2020-04-13 18:18:16 +0900

    common: add extern in MathTools

--- src/banker/common/MathTools.hx
+++ src/banker/common/MathTools.hx
@@ -1,12 +1,12 @@
 package banker.common;
 
 class MathTools {
-	public static inline function minInt(a: Int, b: Int): Int
+	extern public static inline function minInt(a: Int, b: Int): Int
 		return if (a < b) a else b;
 
-	public static inline function maxInt(a: Int, b: Int): Int
+	extern public static inline function maxInt(a: Int, b: Int): Int
 		return if (a < b) b else a;
 
-	public static inline function absoluteInt(n: Int): Int
+	extern public static inline function absoluteInt(n: Int): Int
 		return if (0 <= n) n else -n;
 }
