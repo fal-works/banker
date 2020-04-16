95cc870c63a81ddad3cee4ac237258ae2e2b9117
FAL <falworks.contact@gmail.com>
2020-04-17 01:10:28 +0900

    type_extension: optimize IntExtension.powerOf2()

--- src/banker/type_extension/IntExtension.hx
+++ src/banker/type_extension/IntExtension.hx
@@ -6,7 +6,7 @@ class IntExtension {
 		- On cpp, uses `cpp.NativeMath.idiv()`.
 		- Otherwise just `Std.int(this / denominator)`.
 	**/
-	extern public static inline function divide(_this: Int, denominator: Int): Int {
+	public static extern inline function divide(_this: Int, denominator: Int): Int {
 		#if cpp
 		return cpp.NativeMath.idiv(_this, denominator);
 		#else
@@ -19,7 +19,7 @@ class IntExtension {
 		- On cpp, uses `cpp.NativeMath.imod()`.
 		- Otherwise just `this % denominator`.
 	**/
-	extern public static inline function modulo(_this: Int, denominator: Int): Int {
+	public static extern inline function modulo(_this: Int, denominator: Int): Int {
 		#if cpp
 		return cpp.NativeMath.imod(_this, denominator);
 		#else
@@ -28,28 +28,10 @@ class IntExtension {
 	}
 
 	/**
-		@return `2 ^ this`. If `this` is negative, the result is inaccurate.
+		@return `pow(2, this)`. `this` should be zero or greater.
 	**/
-	public static inline function powerOf2(_this: Int): Int {
-		return switch _this {
-			case 0: 1;
-			case 1: 2;
-			case 2: 4;
-			case 3: 8;
-			case 4: 16;
-			case 5: 32;
-			case 6: 64;
-			case 7: 128;
-			case 8: 256;
-			case 9: 512;
-			case 10: 1024;
-			case 11: 2048;
-			case 12: 4096;
-			case 13: 8192;
-			case 14: 16384;
-			case 15: 32768;
-			case 16: 65536;
-			default: Std.int(Math.pow(2, _this));
-		}
+	public static extern inline function powerOf2(_this: Int): Int {
+		assert(_this >= 0);
+		return 1 << _this;
 	}
 }
