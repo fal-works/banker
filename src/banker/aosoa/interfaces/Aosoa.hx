01b3ecb259a60f71b6f6aee6e4cd40b7eb6a34b3
FAL <falworks.contact@gmail.com>
2020-04-28 11:36:04 +0900

    aosoa: add variables to interface Aosoa

--- src/banker/aosoa/interfaces/Aosoa.hx
+++ src/banker/aosoa/interfaces/Aosoa.hx
@@ -7,7 +7,19 @@ package banker.aosoa.interfaces;
 **/
 interface Aosoa {
 	/**
+		The capacity of each chunk i.e. the length of each vector that a chunk contains.
+	**/
+	public final chunkCapacity: UInt;
+
+	/**
+		The total number of entities in `this` AoSoA, i.e. (chunk capacity) * (chunk count).
+	**/
+	public final capacity: UInt;
+
+	/**
 		Reflects changes after the last synchronization.
 	**/
 	function synchronize(): Void;
 }
+
+// TODO: Make `Aosoa` generic so that it can have `chunks` and `getChunk()`?
