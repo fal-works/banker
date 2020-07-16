7a5523adf7126363213ba416fa29a94008e97883
FAL <falworks.contact@gmail.com>
2020-07-17 00:29:02 +0900

    binary: add comment

--- src/banker/binary/PopReturn.hx
+++ src/banker/binary/PopReturn.hx
@@ -2,6 +2,10 @@ package banker.binary;
 
 /**
 	Return value from pop operations of `ByteStackData`.
+
+	As this class has `inline` constructor, a `PopReturn` instance...
+	- must be directly assigned to a local variable, and
+	- can be passed to or returned from a function only if it is inlined as well.
 **/
 #if !banker_generic_disable
 @:generic
